6a70a222424c73acb58b6a26bcb735f09fcebea6
Scott Bromander <scott@primeacademy.io>
2016-03-21 16:07:28 -0500

    initial commit

---
+++ server/public/assets/scripts/app.js
@@ -0,0 +1,19 @@
+var myApp = angular.module("myApp", []);
+
+myApp.controller("IntroController", ["$scope", function($scope){
+    // $scope.employee = {};
+
+    $scope.employeeArray = [];
+
+    $scope.meow = function(){
+        console.log("Meow");
+    };
+
+    $scope.saveEmployee = function(value){
+        console.log(value);
+        $scope.employeeArray.push(value);
+        $scope.employee = {};
+        console.log($scope.employeeArray);
+    };
+
+}]);
